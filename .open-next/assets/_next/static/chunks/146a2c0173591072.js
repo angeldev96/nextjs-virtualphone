ff793d62c63d5e07f9c2eb951e8a9bd6c9f68faa
Angel <arivalladares2.0@gmail.com>
2025-08-27 20:35:32 -0600

    Refactor project structure for Webflow Cloud compatibility
    - Removed deprecated next.config.mjs file.
    - Updated .gitignore to include .open-next.
    - Deleted unused static assets and cache files to streamline deployment.
    - Added new entries in package.json for improved build and preview processes.
    - Enhanced middleware and server functions for better performance and compatibility.

--- .open-next/assets/_next/static/chunks/146a2c0173591072.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/f8baf2f8d84c334b.js",
-  "static/chunks/turbopack-2c3d3433d7c4aaf8.js"
-])
--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1756347062676,
+    __BUILD_TIMESTAMP_MS__: 1756348391284,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     // The external middleware will use the convertTo function of the `edge` converter
